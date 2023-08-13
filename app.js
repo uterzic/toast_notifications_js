eeed57399daaf2a5dfa0d98fba525a904b7a4468
uterzic <urosterzicbiz@gmail.com>
2023-08-13 22:04:43 +0200

    Added fontawesome link and added icons to messages of toasts

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let toastBox = document.getElementById('toastBox');
-let successMessage = 'Successfully submitted';
-let errorMessage = 'Please fix the error';
-let invalidMessage = 'Invalid input, check again';
+
+let successMessage = '<i class="fa-solid fa-circle-check"></i> Successfully submitted';
+let errorMessage = '<i class="fa-solid fa-circle-xmark"></i> Please fix the error';
+let invalidMessage = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again';
 
 function showToast(message) {
     let toast = document.createElement('div');
