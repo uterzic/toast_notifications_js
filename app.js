e9f51c2bf4a4e189c4ad6aa623e82af6e82c74f5
uterzic <urosterzicbiz@gmail.com>
2023-08-13 21:55:56 +0200

    When you click on button it will show toast with success message

---
+++ app.js
@@ -0,0 +1,9 @@
+let toastBox = document.getElementById('toastBox');
+
+function showToast() {
+    let toast = document.createElement('div');
+    toast.classList.add('toast');
+    toast.innerHTML = 'success';
+
+    toastBox.appendChild(toast);
+}
